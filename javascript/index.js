8150867deb4f94f770d44b9a0fb5f7207e24fd08
anatol-coder <ankawysota@gmail.com>
2024-09-23 17:13:08 +0100

    Added current location

--- javascript/index.js
+++ javascript/index.js
@@ -23,6 +23,10 @@ function showDateAndTime() {
 
 function updateCity(event) {
   let cityTimeZone = event.target.value;
+  if (cityTimeZone === "current") {
+    cityTimeZone = moment.tz.guess();
+  }
+
   let cityTime = moment().tz(cityTimeZone);
   let cityName = cityTimeZone.split("/")[1];
   let citiesElement = document.querySelector("#cities");
