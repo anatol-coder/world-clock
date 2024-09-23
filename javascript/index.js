9d1d5a43154ee6446ba2c7e3505e1f0e112326db
anatol-coder <ankawysota@gmail.com>
2024-09-23 16:38:30 +0100

    Added the functionality for the select

--- javascript/index.js
+++ javascript/index.js
@@ -7,7 +7,7 @@ function showDateAndTime() {
 
   losAngelesDateElement.innerHTML = losAngelesTime.format("MMMM Do YYYY");
   losAngelesTimeElement.innerHTML = losAngelesTime.format(
-    "h:mm:ss:SSS [<small>]A[<small>]"
+    "h:mm:ss [<small>]A[<small>]"
   );
   let warsawElement = document.querySelector("#warsaw");
   let warsawDateElement = warsawElement.querySelector(".date");
@@ -17,8 +17,29 @@ function showDateAndTime() {
 
   warsawDateElement.innerHTML = warsawTime.format("MMMM Do YYYY");
   warsawTimeElement.innerHTML = warsawTime.format(
-    "h:mm:ss:SSS [<small>]A[<small>]"
+    "h:mm:ss [<small>]A[<small>]"
   );
 }
+
+function updateCity(event) {
+  let cityTimeZone = event.target.value;
+  let cityTime = moment().tz(cityTimeZone);
+  let cityName = cityTimeZone.split("/")[1];
+  let citiesElement = document.querySelector("#cities");
+  citiesElement.innerHTML = `
+      <div class="city">
+          <div class="city-date">
+            <h2>${cityName}</h2>
+            <div class="date">${cityTime.format("MMMM Do YYYY")}</div>
+          </div>
+          <div class="time">${cityTime.format(
+            "h:mm:ss"
+          )} <small>${cityTime.format("A")}</small></div>
+        </div>`;
+}
+
 showDateAndTime();
-setInterval(showDateAndTime, 1);
+setInterval(showDateAndTime, 1000);
+
+let citySelectElement = document.querySelector("#city");
+citySelectElement.addEventListener("change", updateCity);
