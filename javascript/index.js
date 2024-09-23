d94c95cb990d1616ce838c02fbebcd49b5f5b666
anatol-coder <ankawysota@gmail.com>
2024-09-23 08:49:02 +0100

    Added js - date and time

--- javascript/index.js
+++ javascript/index.js
@@ -1,1 +1,24 @@
+function showDateAndTime() {
+  let losAngelesElement = document.querySelector("#los-angeles");
+  let losAngelesDateElement = losAngelesElement.querySelector(".date");
+  let losAngelesTimeElement = losAngelesElement.querySelector(".time");
 
+  let losAngelesTime = moment().tz("America/Los_Angeles");
+
+  losAngelesDateElement.innerHTML = losAngelesTime.format("MMMM Do YYYY");
+  losAngelesTimeElement.innerHTML = losAngelesTime.format(
+    "h:mm:ss:SSS [<small>]A[<small>]"
+  );
+  let warsawElement = document.querySelector("#warsaw");
+  let warsawDateElement = warsawElement.querySelector(".date");
+  let warsawTimeElement = warsawElement.querySelector(".time");
+
+  let warsawTime = moment().tz("Europe/Warsaw");
+
+  warsawDateElement.innerHTML = warsawTime.format("MMMM Do YYYY");
+  warsawTimeElement.innerHTML = warsawTime.format(
+    "h:mm:ss:SSS [<small>]A[<small>]"
+  );
+}
+showDateAndTime();
+setInterval(showDateAndTime, 1);
